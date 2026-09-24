0eaa81fc1b5315db95f5d8791ab0b3865426b58f
Nat Chapman <massivephonon.proton.me>
2026-09-23 18:01:16 -0700

    change parse_config to Config::new

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,26 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path): (&str, &str) = parse_config(&args);
+    let config: Config = Config::new(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
     let contents: String =
-        fs::read_to_string(file_path).expect("Should have been able to read the file");
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text: \n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+        Config { query, file_path }
+    }
 }
