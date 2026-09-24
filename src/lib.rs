507d08fbd3748c9e27887fd6f4fc680fc2ce1fcc
Nat Chapman <massivephonon.proton.me>
2026-09-24 08:00:51 -0700

    add failing test

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query: &str = "duct";
+        let contents: &str = "\
+        Rust:
+        saft, fast, productive.
+        Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
